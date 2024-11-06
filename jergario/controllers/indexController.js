b8bd454748ac24df5d7be8d453b02d4a2eeaffb1
caniza1es <juan.canizales@urosario.edu.co>
2024-11-06 17:08:35 -0500

    controlador/enrutador vista index

---
+++ jergario/controllers/indexController.js
@@ -0,0 +1,3 @@
+exports.getIndex = (req,res,next) =>{
+    res.status(200).render("index")
+}
--- jergario/index.js
+++ jergario/index.js
@@ -1,6 +1,11 @@
 const express = require("express")
+const indexRouter = require("./routes/indexRouter")
 
 const app = express()
+
+app.set("views","./views")
+app.set("view engine","ejs")
+app.use("/",indexRouter)
 
 app.listen(3000,()=>{
     console.log("Aplicacion corriendo en http://localhost:3000")
