0474fc35d5ad1c5ff2c32619ae3247db11e4ddd0
caniza1es <juan.canizales@urosario.edu.co>
2024-11-07 18:30:55 -0500

    limpieza

--- jergario/middleware/flashVariables.js
+++ jergario/middleware/flashVariables.js
@@ -1,7 +1,7 @@
 const flashVariablesMiddleware = (req, res, next) => {
     res.locals.error = req.flash('error') || [];
     res.locals.success = req.flash('success') || [];
-    res.locals.useSweetAlert = req.flash('useSweetAlert').length > 0; // Boolean check
+    res.locals.useSweetAlert = req.flash('useSweetAlert').length > 0; 
     next();
 };
 
